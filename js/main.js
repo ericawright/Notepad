e1b2737526eb4b5dc7163980797252f78a8f5928
Erica Wright <ericaewright@gmail.com>
2017-05-11 17:08:46 -0400

    resizable div

---
+++ js/main.js
@@ -0,0 +1,27 @@
+let splitter = document.getElementById('splitter');
+let sidebar = document.getElementById('sidebar');
+
+splitter.addEventListener('mousedown', (event) => {
+  let sidebar_width = sidebar.style.width ? parseInt(sidebar.style.width.slice(0, -2)) : 100;
+  if (event.which !== 1) {
+    return;
+  }
+  let initialX = event.screenX - sidebar_width;
+  let mousemove = (event) => {
+    let newWidth = event.screenX - initialX;
+    if (newWidth < 154) {
+      newWidth = 154;
+    } else if (newWidth > 396) {
+      newWidth = 396;
+    }
+    sidebar.style.width = `${newWidth}px`;
+  };
+
+  let mouseup = (event) => {
+    document.removeEventListener('mousemove', mousemove);
+    document.removeEventListener('mouseup', mouseup);
+  };
+
+  document.addEventListener('mousemove', mousemove);
+  document.addEventListener('mouseup', mouseup);
+});
